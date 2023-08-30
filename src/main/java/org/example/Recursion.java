48f347fa27281d37dfb0312058f5b2ede82ede90
ya_rockstar <yekalashnikov@gmail.com>
2023-08-30 21:34:07 +0300

    add: add tests for Recursion class

--- src/main/java/org/example/Recursion.java
+++ src/main/java/org/example/Recursion.java
@@ -1,2 +1,59 @@
-package org.example;public class Recursion {
+package org.example;
+
+public class Recursion {
+    public static int iterativeFactorial(int number) throws IllegalArgumentException {
+        if (number < 0) {
+            throw new IllegalArgumentException("argument must not be negative");
+        }
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        int result = number;
+
+        for (int i = number - 1; i > 1; i--) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static int recursiveFactorial(int number) throws IllegalArgumentException {
+        if (number < 0) {
+            throw new IllegalArgumentException("argument must not be negative");
+        }
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        return number * recursiveFactorial(number - 1);
+    }
+
+
+    /*
+    * Хвостовая рекурсия — частный случай рекурсии, при котором любой рекурсивный вызов является последней операцией
+    * перед возвратом из функции. Подобный вид рекурсии может быть оптимизирован компилятором и заменён на итерацию.
+    * В Java компилятор не оптимизирует хвостовую рекурсию.
+    */
+    public static int tailRecursiveFactorial(int number) throws IllegalArgumentException {
+        if (number < 0) {
+            throw new IllegalArgumentException("argument must not be negative");
+        }
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        return innerTailRecursiveFactorial(number, number);
+    }
+
+    private static int innerTailRecursiveFactorial(int number, int result) {
+        if (number == 2) {
+            return result;
+        }
+
+        return innerTailRecursiveFactorial(number - 1, result * (number - 1));
+    }
 }
--- src/test/java/RecursionTest.java
+++ src/test/java/RecursionTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class RecursionTest {
+import org.example.Recursion;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RecursionTest {
+    @Test
+    public void TestIterativeFactorial() {
+        assertEquals(Recursion.iterativeFactorial(0), 1);
+        assertEquals(Recursion.iterativeFactorial(1), 1);
+        assertEquals(Recursion.iterativeFactorial(5), 120);
+        assertEquals(Recursion.iterativeFactorial(10), 3628800);
+    }
+
+    @Test
+    public void TestRecursiveFactorial() {
+        assertEquals(Recursion.recursiveFactorial(0), 1);
+        assertEquals(Recursion.recursiveFactorial(1), 1);
+        assertEquals(Recursion.recursiveFactorial(5), 120);
+        assertEquals(Recursion.recursiveFactorial(10), 3628800);
+    }
+
+    @Test
+    public void TestTailRecursiveFactorial() {
+        assertEquals(Recursion.tailRecursiveFactorial(0), 1);
+        assertEquals(Recursion.tailRecursiveFactorial(1), 1);
+        assertEquals(Recursion.tailRecursiveFactorial(5), 120);
+        assertEquals(Recursion.tailRecursiveFactorial(10), 3628800);
+    }
 }
